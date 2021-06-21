a40262cefa4a38b9de5fcca9a3b1de4bb14457fd
MichalWes <massive7m@gmail.com>
2021-06-21 17:33:54 +0200

    First commit

---
+++ 20.1.py
@@ -0,0 +1,22 @@
+class User:
+    age = 0
+
+
+seba = User()
+mirek = User()
+andrzej = User()
+
+seba.age = 16
+mirek.age = 24
+
+seba.age = 10
+age = 40
+
+print(seba.age)
+print(mirek.age)
+print(andrzej.age)
+print(age)
+
+x = 5.0
+
+print(x.is_integer())
