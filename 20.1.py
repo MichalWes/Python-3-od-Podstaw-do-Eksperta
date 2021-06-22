c214d7d8c133389ec15e14861a99dced848a9c35
MichalWes <massive7m@gmail.com>
2021-06-22 12:39:22 +0200

    Rocket improved

--- 20.1.py
+++ 20.1.py
@@ -4,27 +4,29 @@ import random
 
 class Rocket:
 
-    def __init__(self, name, height, firePower, prodDate, base):
+    def __init__(self, name, height, speed, prodDate, base):
         print("Rocket initialized")
         self.name = name
         self.height = height
-        self.firePower = firePower
+        self.speed = speed
         self.prodDate = prodDate
         self.base = base
-        self.posVert = 0
+        self.altitude = 0
+
+    def moveUp(self, speed):
+        self.altitude += speed * random.randrange(0, 10)
 
 
 RocketList = {
-    Rocket("ORP Pułaski", 40, 1000, 2005, "Katowice"),
-    Rocket("ORP Sienkiewicz", 35, 1400, 2011, "Gdynia"),
-    Rocket("ORP Jan Paweł II", 45, 6000, 1995, "Wadowice"),
-    Rocket("ORP Kaczynski I", 15, 10, 2010, "Smolensk"),
-    Rocket("ORP Kaczynski II", 10, 5, 2018, "Warszawa")
+    Rocket("ORP Pułaski", 40, 300, 2005, "Katowice"),
+    Rocket("ORP Sienkiewicz", 35, 1100, 2011, "Gdynia"),
+    Rocket("ORP Jan Paweł II", 45, 2200, 1995, "Wadowice"),
+    Rocket("ORP Kaczynski I", 15, 500, 2010, "Smolensk"),
+    Rocket("ORP Kaczynski II", 10, 100, 2018, "Warszawa")
 }
-
 
 for element in RocketList:
     for i in range(0, 10):
-        element.posVert += random.randrange(0, 1000)
+        element.moveUp(element.speed)
     print("Rakieta " + element.name +
-          " jest na wysokości " + str(element.posVert) + "m")
+          " jest na wysokości " + str(element.altitude) + "m")
