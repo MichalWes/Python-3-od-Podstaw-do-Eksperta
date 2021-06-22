ef7488c7d07a744da5ad925ffa9457af3984c575
MichalWes <massive7m@gmail.com>
2021-06-22 10:18:02 +0200

    Next lesson

--- 20.1.py
+++ 20.1.py
@@ -2,26 +2,23 @@ from types import ClassMethodDescriptorType
 
 
 class User:
-    age = 0
-    name = ""
-
     def print_age(self):
         print(self.name, "ma", self.age, "lat")
+
+    def __init__(self, age, name):
+        print("Jestem inicjalizatorem")
+        self.age = age
+        self.name = name
 
     def name(args):
         pass
 
 
-user1 = User()
-user2 = User()
-user3 = User()
+user1 = User(34, "Arek")
+user2 = User(24, "Mirek")
+user3 = User(23, "Michal")
 UserList = [user1, user2, user3]
 
-user1.name = "Arek"
-user1.age = 27
-
-user2.name = "Witek"
-user2.age = 34
 
 UserList[1].print_age()
 # user2.print_age()
