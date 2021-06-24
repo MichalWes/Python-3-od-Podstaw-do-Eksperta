c1a1cf3256acce8a49db81b859d6b4c43f847633
MichalWes <massive7m@gmail.com>
2021-06-24 10:44:20 +0200

    Exercise Accounts

---
+++ Exercise203E.py
@@ -0,0 +1,11 @@
+from Exercise203S import BankAccount
+
+accounts = [BankAccount() for _ in range(10)]
+
+accounts[0].deductMoneyFromBalance(200)
+accounts[0].addMoneyToBalance(2205)
+accounts[0].checkAccountBalance()
+
+""" for account in accounts:
+    print(account.id)
+    print(account.balance) """
---
+++ Exercise203S.py
@@ -0,0 +1,27 @@
+class BankAccount:
+    nextId = 10000
+
+    def __init__(self, name="Domyślny Użytkownik", balance=600):
+        """Constructor
+        """
+        print("Użytkownik zainicjalizowany")
+        self.name = name
+        self.balance = balance
+        self.id = BankAccount.nextId
+        BankAccount.nextId += 1
+
+    def addMoneyToBalance(self, amount):
+        self.balance += amount
+        print("Wpłacono: " + str(amount) + "zł")
+        print("Na koncie jest teraz: " + str(self.balance) + "zł")
+
+    def deductMoneyFromBalance(self, amount):
+        if self.balance > amount:
+            self.balance -= amount
+            print("Wypłacono: " + str(amount) + "zł")
+            print("Pozostało: " + str(self.balance) + "zł")
+        else:
+            print("Za mało pieniędzy na koncie")
+
+    def checkAccountBalance(self):
+        print("Stan konta to: " + str(self.balance) + "zł")
