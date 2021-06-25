e792aea46ed3d9e34fc074b39246f42afcf59187
MichalWes <massive7m@gmail.com>
2021-06-25 11:21:02 +0200

    +Volume

--- Exercise204.py
+++ Exercise204.py
@@ -16,6 +16,9 @@ class Cube(Square):
     def areaC(self):
         return super().area() * 6
 
+    def volume(self):
+        return super().area() * self.a
+
 
 kwadrat = Square(6)
 print(kwadrat.a)
@@ -24,3 +27,4 @@ print(kwadrat.area())
 
 szescian = Cube(4)
 print(szescian.areaC())
+print(szescian.volume())
