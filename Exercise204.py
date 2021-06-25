6be9c1a9c7d15550c7aecb77a0809ff9932c46d5
MichalWes <massive7m@gmail.com>
2021-06-25 11:17:18 +0200

    Inhertiance cube, square, rectangle

---
+++ Exercise204.py
@@ -0,0 +1,26 @@
+class Rectangle:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def area(self):
+        return self.a * self.b
+
+
+class Square(Rectangle):
+    def __init__(self, a):
+        super().__init__(a, a)
+
+
+class Cube(Square):
+    def areaC(self):
+        return super().area() * 6
+
+
+kwadrat = Square(6)
+print(kwadrat.a)
+print(kwadrat.b)
+print(kwadrat.area())
+
+szescian = Cube(4)
+print(szescian.areaC())
